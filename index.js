69cda9a4ee1fde5907d53744c732ad6171771478
JT Caldwell <jtcodewell@gmail.com>
2018-03-21 17:59:52 -0500

    run test

--- index.js
+++ index.js
@@ -7,24 +7,24 @@
 const invoke = () =>
   // invoke the following function so that
   // it returns the value 0
-  (() => 0)
+  (() => 0) ()
 
 
 const helloWorld = () =>
   // rewrite the following block so
   // the string is returned
-  { "Hello World" }
+  "Hello World"
 
 
 const whatIsIt = () =>
   // make it true! what does void 0 equal?
-  void 0 === ''
+  void 0 === void 0
 
 
 const commaFTW = () =>
   // change one + to a comma so the
   // function returns 7
-  (1 + 1 + 2 + 4)
+  (1, 1 + 2 + 4)
 
 
 /* 
@@ -34,41 +34,40 @@ const commaFTW = () =>
  */
 const buildingBlocks = () => (
   // change the next line to use block syntax
-  () => 'passing this test like a boss'
+  () => 1 + 1, 
+        2 + 2
 )()
 
 
 const writeAFunction = () => (
   // replace the empty string with a function
   // that returns any non-empty string
-  ''
+  () => 1 + 1
 )()
 
 
 const whatDoesItDo = () =>
   // what does this function evaluate to?
   // replace the empty string your answer
-  (() => {})() === ''
+  (() => {})() === undefined
 
 
 const returnSomething = () =>
   // this is currently the same function as
   // the previous one; this time, make the
   // function return something
-  (() => {})()
+  (() => "Hello, JavaScript")()
 
 
 const inlineComment = () =>
-  // 'uncomment this line'
-  'change this line to an inline comment'
+  // 'change this line to an inline comment'
 
 
-const multiLineNoInline = () => {
-  // keep these lines commented, but
-  // change the syntax to a multiline comment
-  // and remove the inline syntax
-  return // remove this comment & return a value
-}
+  const multiLineNoInline = () => 
+  /* keep these lines commented, but
+   change the syntax to a multiline comment
+   and remove the inline syntax */
+    "Hello World"
 
 // that's all!
 
